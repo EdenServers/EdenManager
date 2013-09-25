ba1d70ba5018921d0aee68e3d09eabb3f3b97045
Thomas Geselle <raitomm@gmail.com>
2013-09-25 19:33:35 +0200

    Merge fix

--- modules/packets/packet.rb
+++ modules/packets/packet.rb
@@ -5,18 +5,6 @@ class Packet < EM::Connection
     length = data.length
     if length >= 2
       packet = JSON.parse(data)
-<<<<<<< HEAD
-      case packet['packet_id']
-        when 1 #Install
-          installation = ScrollInstaller.new(packet['scroll_name'], packet['scroll_options'])
-          id = installation.install
-          send_data JSON.generate({status: 'OK', service_id: id}) + "\n" #Don't forget this shit again !
-        when 2 #Start
-          ServiceManager.start_service(packet['service_id'])
-          send_data JSON.generate({status: 'OK'}) + "\n"
-        else
-          Console.show "Unknown packet : #{packet}"
-=======
       if checkMasterKey packet['master_key']
         case packet['packet_request']
           when 'install' #Install
@@ -33,7 +21,6 @@ class Packet < EM::Connection
       else
         Console.show "Master key is invalid : #{packet}", 'error'
         close_connection
->>>>>>> quelques-améliorations
       end
     else
       Console.show "Just got a wrong packet with length : #{length}", 'error'

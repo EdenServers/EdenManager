096cf69d6beedf2f75b5a468154fc574f712dfca
Dernise <dernise@live.fr>
2013-10-02 21:12:33 +0200

    Correcting sofiane's scrolls

--- scrolls/Php/php.rb
+++ scrolls/Php/php.rb
@@ -1,4 +1,4 @@
-class PHP < Scroll
+class Php < Scroll
   def initialize(options = {})
     self.name = 'PHP'
     self.author = 'SofianeG'
@@ -13,8 +13,8 @@ class PHP < Scroll
   end
 
   def install
-    add_source ("deb http://packages.dotdeb.org wheezy-php55 all")
-    add_source ("deb-src http://packages.dotdeb.org wheezy-php55 all")
+    add_source('deb http://packages.dotdeb.org wheezy-php55 all')
+    add_source('deb-src http://packages.dotdeb.org wheezy-php55 all')
     System.apt_get('update')
     System.apt_get('install', 'libapache2-mod-php5')
     register('/etc/init.d/apache2 restart')

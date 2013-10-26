b9c73f6bf83ecb67eb2bc424b532f5b7f6d57c6a
Maxence Henneron <dernise@live.fr>
2013-10-26 16:44:32 +0200

    Adding a log to password change

--- modules/packets/packet.rb
+++ modules/packets/packet.rb
@@ -38,7 +38,7 @@ class Packet < EM::Connection
           when 'get_installed_services'
             send_data JSON.generate({status: 'OK', services: ServiceManager.get_installed_services}) + "\n"
           when 'change_root_password'
-            if(System.change_password('root', packet['new_password']))
+            if System.change_password('root', packet['new_password'])
               send_data JSON.generate({status: 'OK'}) + "\n"
             end
           else

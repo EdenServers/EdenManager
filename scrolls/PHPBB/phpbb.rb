367f14605072a0a43c1768c209ef268e1b25aaeb
SofianeG <sofianegargouri51@gmail.com>
2013-11-02 19:10:14 +0100

    Moving new scrolls

---
+++ scrolls/PHPBB/phpbb.rb
@@ -0,0 +1,24 @@
+class PHPBB < Scroll
+  def initialize(name = 'PHPBB', options = {})
+    self.name = name
+    self.type = 'PHPBB'
+    self.author = 'SofianeG'
+    self.version = '3.0.11'
+    self.homepage = 'http://wwww.edenservers.fr'
+    self.url = 'http://www.phpbb.fr/telechargement/phpBB-3.0.11-fr.zip'
+    self.dependable = false
+    super
+  end
+
+  def set_dependencies
+    add_dep('Apache')
+    add_dep('PHP')
+    add_dep('MySQL')
+  end
+
+  def install
+    download('phpBB-3.0.11-fr.zip')
+    copy(self.install_folder, 'phpBB-3.0.11-fr.zip')
+    extract('zip', 'phpBB-3.0.11-fr.zip', self.install_folder)
+  end
+end

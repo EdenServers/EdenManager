aaf2087339da9590056549736d124ac9e7a6810c
Maxence Henneron <dernise@live.fr>
2013-12-23 17:28:57 +0100

    Starting Steamcmd as EdenManager

--- scrolls/Steamcmd/SteamcmdController.rb
+++ scrolls/Steamcmd/SteamcmdController.rb
@@ -7,6 +7,14 @@ class SteamcmdController < Controller
   end
 
   def install_game(game_id, directory)
-    system("su EdenManager && ./EdenApps/Steamcmd/Steamcmd/steamcmd.sh +login anonymous +force_install_dir #{directory} +app_update #{game_id} validate +quit")
+    p1 = Process.fork {
+        if ::Process::Sys.geteuid == 0
+          uid_num = Etc.getpwnam('EdenManager').uid
+          ::Process::Sys.setuid(uid_num) if uid
+        end
+        system("./EdenApps/Steamcmd/Steamcmd/steamcmd.sh +login anonymous +force_install_dir #{directory} +app_update #{game_id} validate +quit")
+        exit
+    }
+    Process.detach(p1)
   end
 end
